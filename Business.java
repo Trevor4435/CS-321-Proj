706724cd5fb45b1f4784cf8e831aa0af0c33caff
Trevor4435 <wernerkid35@gmail.com>
2023-10-04 14:34:34 -0400

    Tiny update

--- Business.java
+++ Business.java
@@ -1,5 +1,13 @@
 public class Business{
+
     public static void main(String[] args){
         System.out.println("Hello World!");
     }
+
+    public int CreateNewBO(String[] input){
+
+
+        return 1;
+    }
+
 }
