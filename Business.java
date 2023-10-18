25047e2a376c829637dcc03dd3e9678a3362559b
NicoleJ18546 <nicolejosephk18546@gmail.com>
2023-10-18 13:41:34 -0400

    added getters

--- Business.java
+++ Business.java
@@ -5,5 +5,32 @@ public class Business{
 
         return null;
     }
+    
+    protected static String getAlienNumber(){
+        return "number";
+    }
 
+    protected static String getFirstName(){
+        return "supercalifragilisticexpialidocious";
+    }
+
+    protected static String getLastName(){
+        return "";
+    }
+
+    protected static String getMiddleName(){
+        return " ";
+    }
+
+    protected static String getAddress(){
+        return "space";
+    }
+
+    protected static String getRefNum(){
+        return "-1";
+    }
+
+    
+
+ 
 }
