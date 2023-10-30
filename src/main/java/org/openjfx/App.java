bcb70d968d15328b0d9561abde322d4d0cb5b57e
Trevor4435 <wernerkid35@gmail.com>
2023-10-30 18:34:50 -0400

    Most data entry comms with BO set up, just need BO and WF to gain functionality.

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -1,6 +1,7 @@
 package org.openjfx;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -16,6 +17,7 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     protected static Stage permStage;
+    protected static int refNumber = 1;
 
     @Override
     public void start(Stage stage) {
@@ -34,30 +36,27 @@ public class App extends Application {
 
 
         // Create the welcome instructions and display them.
-        Text welcome = new Text();
-        welcome.setText("Please selected your desired service.");
+        Text welcome = new Text("Please selected your desired service.");
         grid.add(welcome, 0, 0);
 
-
         // Create the select buttons and display in a stacked list.
-        Button dataEntry = new Button();
-        dataEntry.setText("Data Entry");
+        Button dataEntry = new Button("Data Entry");
         grid.add(dataEntry, 0, 1);
 
-        Button review = new Button();
-        review.setText("Review");
+        Button review = new Button("Review");
         grid.add(review, 0, 2);
 
-        Button approve = new Button();
-        approve.setText("Approval");
+        Button approve = new Button("Approval");
         grid.add(approve, 0, 3);
+
+        Button exit = new Button("Exit");
+        grid.add(exit, 1, 4);
 
         // Set up the event handlerss for the buttons to call their respective classes
         dataEntry.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event){
                 DataEntry data = new DataEntry();
-                grid.getChildren().clear();
                 stage.hide();
                 data.showScreen();
             }
@@ -74,6 +73,14 @@ public class App extends Application {
             @Override
             public void handle(ActionEvent event){
                 
+            }
+        });
+
+        // Just a way to exit if the user is too lazy to use the X button.
+        exit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event){
+                Platform.exit();
             }
         });
 
--- src/main/java/org/openjfx/DataEntry.java
+++ src/main/java/org/openjfx/DataEntry.java
@@ -1,5 +1,7 @@
 package org.openjfx;
 
+import java.util.Arrays;
+
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -9,11 +11,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class DataEntry {
 
     public void showScreen(){
+
+        int ref = App.refNumber;
 
         // Take the stage declared in App.java and prepare a fresh scene to go into it before being showed again.
         Stage stage = App.permStage;
@@ -23,18 +28,111 @@ public class DataEntry {
         grid.setVgap(10);
         grid.setPadding(new Insets(25,25,25,25));
 
+        // Declare the label and field for an alien number to be entered.
         Label aNumber = new Label("Alien Number: ");
         grid.add(aNumber, 0, 0);
         TextField alienNumber = new TextField();
         grid.add(alienNumber, 1, 0);
 
+
+        Label fName = new Label("First Name: ");
+        grid.add(fName, 0, 1);
+        TextField firstName = new TextField();
+        grid.add(firstName, 1, 1);
+
+        // Dont forget this is meant to be split and handled as an Array
+        Label mNames = new Label("Middle Name: \n(Seperate with a ',' or leave empty)");
+        grid.add(mNames, 0, 2);
+        TextField middleNames = new TextField();
+        grid.add(middleNames, 1, 2);
+
+        Label lName = new Label("Last Name: ");
+        grid.add(lName, 0, 3);
+        TextField lastName = new TextField();
+        grid.add(lastName, 1, 3);
+
+        Label bDay = new Label("Date of Birth: \n (Please enter in MM/DD/YYYY)");
+        grid.add(bDay, 0, 4);
+        TextField DOB = new TextField();
+        grid.add(DOB, 1, 4);
+
+        Label mAddress = new Label("Mailing Address for Delivery: ");
+        grid.add(mAddress, 0, 5);
+        TextField mailingAddress = new TextField();
+        grid.add(mailingAddress, 1, 5);
+
+        Label error = new Label();
+        grid.add(error, 0, 6);
+        error.setTextFill(Color.RED);
+
+
+        Button submit = new Button("Submit");
+        grid.add(submit, 2, 9);
+
         Button exit = new Button("Exit");
-        grid.add(exit, 0, 5);
+        grid.add(exit, 0, 9);
+
 
         exit.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event){
                 Platform.exit();
+            }
+        });
+
+        // This is out here to stop a possibility of a memory leak my mashing submit
+        Business BO = Business.createNewBO();
+
+        
+        // This is where we are finally going to start checking for input.
+        // These are very basic checks just to make sure input exists where it's required.
+        submit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event){
+                // Reset the error text each time submit is pressed
+                error.setText("");
+
+                // Only the middle name is permitted to be empty, no other field may be empty
+                if(alienNumber.getText().isEmpty()){
+                    error.setText("ERROR: Empty Alien Number!");
+                }
+                else if(firstName.getText().isEmpty()){
+                    error.setText("ERROR: Empty First Name!");
+                }
+                else if(lastName.getText().isEmpty()){
+                    error.setText("ERROR: Empty Last Name!");
+                }
+                else if(DOB.getText().isEmpty()){
+                    error.setText("ERROR: Empty Date of Birth!");
+                }
+                else if(mailingAddress.getText().isEmpty()){
+                    error.setText("ERROR: Empty Mailing Address!");
+                }
+
+                // TODO Add in parsing for the error returned by the BO validation metric.
+
+
+                BO.setAlienNumber(alienNumber.getText());
+                BO.setRefNumber(Integer.toString(ref));
+                BO.setFirstName(firstName.getText());
+                BO.setLastName(lastName.getText());
+
+                // Split on ", " to permit middle names with spaces
+                BO.setMiddleName(middleNames.getText().split(", "));
+                
+                BO.setAddress(mailingAddress.getText());
+                BO.setDOB(DOB.getText());
+
+                switch (BO.validate()) {
+                    case -1:
+                        
+                        break;
+                
+                    default:
+                        System.out.println("ERROR ERROR, SWITCH DEFAULT OCCURANCE");
+                }
+                
+                
             }
         });
 
